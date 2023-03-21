79a214abc4d54a7254209de928e964938ca4826a
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-21 15:58:04 -0700

    Create Header and Empty components

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,4 +145,14 @@ storiesOf("Appointment", module) // module is a webpack module, which is availab
     <Appointment
       time={"12pm"}
     />
-  ));
+  ))
+  .add("Header", () => (
+    <Header
+      time={"12pm"}
+    />
+  ))
+  .add("Empty", () => (
+    <Empty
+      onAdd={action("onAdd")}
+    />
+  ))
