63632b9416161e1bb0a3812ac1f9715684d40917
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-21 20:34:59 -0700

    Add Appointment component to the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-
 import "components/Application.scss";
 import DayList from "./DayList";
-import InterviewerList from "./InterviewerList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -22,20 +21,60 @@ const days = [
   },
 ];
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  },
+  "6": {
+    id: 6,
+    time: "5pm",
+  }
+};
+
+const appointmentList = Object.values(appointments).map((appointment) => {
+  return (
+    <Appointment
+      key={appointment.id}
+      {...appointment}
+    />
+  )
+})
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
-  const [interviewer, setInterviewer] = useState(0);
-
-  console.log(day);
-  console.log(interviewer);
 
   return (
     <main className="layout">
@@ -62,17 +101,11 @@ export default function Application(props) {
       </section>
 
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
       </section>
       
     </main>
   );
 }
 
-// To include:
 
-{/* <InterviewerList
-interviewers={interviewers}
-value={interviewer}
-onChange={setInterviewer}
-/> */}
