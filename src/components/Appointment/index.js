f592b969b826602a32178fcb71d71f22fa14627c
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-21 20:07:06 -0700

    Apply conditional rendering to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment (props) {
-
+  
   return (
     <article className="appointment">
+      <Header
+        time={props.time}
+      />
+
+      <>
+        {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+        :
+        <Empty/>}
+      </>
+
     </article>
   );
 }
