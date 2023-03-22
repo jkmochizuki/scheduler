613305c63a09fa7ab0d8253876649b2cf7e16bed
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-21 18:46:38 -0700

    Adjust props and state in the Appointment Form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,62 @@
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+import React, { useState } from "react";
+
+export default function Form (props) {
+
+  /*
+  props
+  student: string
+  interviewers: array
+  interviewer: number
+  onSave: function
+  onCancel: function
+  */
+
+  const [student, setStudent] = useState(props.student || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function () {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = function () {
+    reset();
+    props.onCancel();
+  }
+
+  const save = function () {
+    props.onSave(student, interviewer);
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+
+
+}
