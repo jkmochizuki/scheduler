af92ae5d2c0cb741e33a6cb921507577181f85c6
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-23 14:32:54 -0700

    Create a back function within useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,11 +3,26 @@ import { useState } from "react";
 /*  takes an initial argument to set the mode state and returns an object { mode, transition } */
 export default function useVisualMode (initial) {
   const [mode, setMode] = useState(initial);
+
+  /* to keep track of the history of the modes, so we can go backwards */
+  const [history, setHistory] = useState([initial]);
  
   /* takes in a new mode and update the mode state with the new value */
-  const transition = function(mode) {
-    setMode(prev => mode)
+  const transition = function(newMode) {
+    setMode(newMode)
+    setHistory(prev => [...prev, newMode])
   }
-  return { mode, transition };
+
+  /* sets the mode to the previous item in the history array */
+  const back = function() {
+    if (history.length > 1) { // to not allow the user to go back past the initial mode
+      const newHistory = [...history];
+      newHistory.pop(); // removes the last mode in the history array
+      
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  }
+  return { mode, history, transition, back };
 }
 
