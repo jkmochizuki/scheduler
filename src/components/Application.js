c3bf4516b5aa2dc0f0b85c026a939ad05c0ae678
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-23 18:29:23 -0700

    Refactor Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,7 +8,7 @@ import { GET_APPOINTMENTS, GET_DAYS, GET_INTERVIEWERS } from "helpers/constants"
 
 export default function Application() {
   
-  /* combines all of the state into a single object */
+  /* state */
   const [state, setState] = useState({
     day: "",
     days: [],
@@ -19,17 +19,22 @@ export default function Application() {
   /* action to update day state */
   const setDay = day => setState(prev => ({ ...prev, day }));
 
-  /* request as a side effect to update the component when days, appointments, and interviewers data is retrieved */
+  /* hook that fires only after the initial render, to load days, appointments, and interviewers */
   useEffect(() => {
     Promise.all([
       axios.get(GET_DAYS),
       axios.get(GET_APPOINTMENTS),
       axios.get(GET_INTERVIEWERS)
     ]).then((all) => {
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data})) // set the days, appointments, and interviewers state at the same time
+        setState(prev => ({ 
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        })) 
       })
       .catch((err) => {
-        console.log(err.response);
+        console.log('error:', err);
       });
       /* empty dependency because API request does not depend on the state or props of this component => never rerun this request */
   }, []) 
@@ -39,7 +44,6 @@ export default function Application() {
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-
     return (
       <Appointment
         key={appointment.id}
