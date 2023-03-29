ebdc4621aa181b8fad073f9b128756cc1237452b
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-03-28 17:24:52 -0700

    Implement mock functions for axios.get()

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,28 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-describe("Appointment tests", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+describe("Application tests", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    
+    /* returns a promise that resolves when the callback returns a truthy value and rejects after a time out when it cannot find the specified text */
+      return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    })
   });
-})
+
+});
+
+
+/*
+We will use containers to find specific DOM nodes.
+We will chain promises to handle asynchronous testing.
+We will override mock implementations for specific tests.
+We will use setup and teardown functions provided by Jest to perform common tasks.
+*/
+
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,6 +1,6 @@
-export const GET_DAYS = "http://localhost:8001/api/days";
-export const GET_APPOINTMENTS = "http://localhost:8001/api/appointments";
-export const GET_INTERVIEWERS = "http://localhost:8001/api/interviewers";
+export const GET_DAYS = "/api/days";
+export const GET_APPOINTMENTS = "/api/appointments";
+export const GET_INTERVIEWERS = "/api/interviewers";
 export const EMPTY = "EMPTY";
 export const SHOW = "SHOW";
 export const CREATE = "CREATE";
